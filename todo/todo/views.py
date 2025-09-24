5e5f43f083f0dea90620bc261d3fa88bdf5ec7c5
han-son-03 <rkdgksths11@naver.com>
2025-09-24 18:35:50 +0900

    views todolist 부분 로그인된 사용자에 맞춰 게시글 보이도록 작성

--- todo/todo/views.py
+++ todo/todo/views.py
@@ -1,14 +1,27 @@
+from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
+from django.db.models import Q
 from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 
+from todo.forms import TodoForm
 from todo.models import Todo
 
 
 def todo_list(request):
-    todo_list = Todo.objects.all().values_list('id', 'title')
-    result = [{'id': todo[0], 'title': todo[1]} for i, todo in enumerate(todo_list)]
+    todo_list = Todo.objects.filter(user=request.user).order_by('created_at')
+    q = request.GET.get('q')
+    if q:
+        todo_list = todo_list.filter(title__icontains=q) | Q(description__icontains=q)
+    paginator = Paginator(todo_list, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {
+        'page_obj': page_obj,
+    }
 
-    return render(request, 'todo_list.html', {'data': result})
+    return render(request, 'todo_list.html', context)
 
 
 def todo_info(request, id):
@@ -23,4 +36,16 @@ def todo_info(request, id):
         }
         return render(request, 'todo_info.html', {'data': info})
     except Todo.DoesNotExist:
+
         raise Http404("Todo does not exist")
+
+
+# @login_required
+# def todo_create(request):
+#     form = TodoForm(request.POST or None)
+#     if form.is_valid():
+#         todo = form.save(commit=False)
+#         todo.user = request.user
+#         users.save()
+#             return redirect(reverse('todo_info', kwargs={'todo_id': todo.pk}))
+
