a31b488f2f78da77acd9530cbc264c6d4cf2352f
han-son-03 <rkdgksths11@naver.com>
2025-09-24 14:00:28 +0900

    ✨ Add authentication URLs (login, logout, signup)

--- todo/config/urls.py
+++ todo/config/urls.py
@@ -15,12 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
+from django.urls import path, include
 from todo.views import todo_list, todo_info
+form user import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('todo/', todo_list, name='todo_list'),  # 추가
-    path('todo/<int:id>/', todo_info, name='todo_info'),  # 추가
+    path('todo/', todo_list, name='todo_list'),
+    path('todo/<int:id>/', todo_info, name='todo_info'),
+
+    #auth
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/logout/', user_views.logout, name='logout'),
+    path('accounts/signup/', user_views.signup, name='signup'),
 ]
